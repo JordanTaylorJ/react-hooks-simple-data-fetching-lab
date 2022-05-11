262b2b8def95c635c016899089f1449e7e222c23
JordanJoseph <jordant.joseph@outlook.com>
2022-05-10 18:37:44 -0600

    commit tree 8e0ff8a7c272da3f7e2a6268c7635738e8cc9ec8

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,23 @@
+import React, {useState, useEffect} from "react";
+
 // create your App component here
+function App(){
+    const [dogImage, setDogImage] = useState((null));
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+            .then((res) => res.json())
+            .then((data) => {
+                setDogImage(data.message)
+            });
+    }, []);
+
+
+    if (!dogImage){
+        return (<p>Loading...</p>);
+    } else {
+        return(<img src={dogImage} alt="A Random Dog" />);
+    }
+}
+
+export default App;
